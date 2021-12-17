e7df15b418dedc20b2e2abf1075ca1d8e5fa1376
Dmitrii <zeionara@gmail.com>
2021-12-17 16:44:31 +0300

    Implemented blazegraph query sending

--- main.exs
+++ main.exs
@@ -6,9 +6,23 @@ verbose_header = false
 output = nil
 names = ["premise", "statement", "conclusion"]
 
-Cartex.Cli.make_meta_query(
-  %Optimus.ParseResult{options: %{limit: limit, offset: offset, n: n, core: core, output: output}, flags: %{verbose_header: verbose_header}, unknown: names}
-)
+blazegraph_host = "172.16.55.163"
+
+# Cartex.Cli.make_meta_query(
+#   %Optimus.ParseResult{options: %{limit: limit, offset: offset, n: n, core: core, output: output}, flags: %{verbose_header: verbose_header}, unknown: names}
+# )
+
+HTTPoison.start
+case HTTPoison.post(
+  "http://#{blazegraph_host}:9999/bigdata/namespace/kb/sparql",
+  # "query=select+(count(*)+as+%3Fcount)+%7B+%3Fh+%3Fr+%3Ft+%7D%0D%0A",
+  # "select+(count(*)+as+%3Fcount)+%7B+%3Fh+%3Fr+%3Ft+%7D%0D%0A"
+  # URITools.encode_www_form("select (count(*) as ?count) { ?h ?r ?t }\r\n", ["(", ")", "*"]),
+  "query=#{URITools.encode_www_form("select (count(*) as ?count) { ?h ?r ?t }", ["(", ")", "*"])}",
+  [{"Accept", "application/sparql-results+json"}, {"Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"}]
+) do
+  {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> IO.puts body
+end
 
 # Cartex.make_all_handlers(4, ["foo", "bar", "baz", "qux"], "<<core>>") |> IO.inspect
 # Cartex.make_all_handlers(4, ["foo", "bar", "baz", "qux"], "<<core>>", as_string: true) |> IO.puts
